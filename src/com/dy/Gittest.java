6da2c4dc8b77ee9b543ca28635ba7d7369e684cd
ChenDeYou <1780080951@qq.com>
2020-08-04 22:32:28 +0800

    我刚刚的错误了

---
+++ src/com/dy/Gittest.java
@@ -0,0 +1,19 @@
+package com.dy;
+
+public class Gittest {
+
+    public static void main(String[] args) {
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+        System.out.println("你是一只猪");
+
+    }
+}
